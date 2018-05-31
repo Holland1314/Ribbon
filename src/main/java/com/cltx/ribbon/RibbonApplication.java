622c29389702a9e06a8aeeb2de1c6e5f32a7a621
shileichao <shileichao@auto-link.local>
2018-05-31 09:54:30 +0800

    init ribbon

---
+++ src/main/java/com/cltx/ribbon/RibbonApplication.java
@@ -0,0 +1,40 @@
+package com.cltx.ribbon;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//向服务注册中心注册
+@EnableDiscoveryClient
+
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RibbonApplication.class, args);
+	}
+
+	/**
+	 *向程序的ioc注入一个bean：restTemplate
+	 * 通过@LoadBalanced注解表明这个restTemplate开启负载均衡功能
+	 * @return
+	 */
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	@Bean
+	public IRule ribbonRule(){
+		return new RandomRule();//实例化与配置文件对应的策略类
+	}
+}
---
+++ src/main/java/com/cltx/ribbon/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.cltx.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by shileichao on 2018/5/30.
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam("id") String id) {
+
+        // ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://SPRINGBOOT-CLIENT/user", String.class);
+        //return  restTemplate.getForObject("http://127.0.0.1:9091/user/findById?id="+id,String.class);
+        return restTemplate.getForObject("http://SPRINGBOOT-CLIENT/user/findById?id=" + id, String.class);
+
+    }
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
+    public String getUser(@RequestParam("id") String id) {
+        this.loadBalancerClient.choose("SPRINGBOOT-CLIENT");
+        return restTemplate.getForEntity("http://SPRINGBOOT-CLIENT/user/findById?id=" + id, String.class).getBody();
+    }
+}
